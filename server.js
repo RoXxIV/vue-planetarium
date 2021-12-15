3a6f51c1c41d2fdeeea69c5768e641c30aa7c5af
evan <evan.hermier@gmail.com>
2021-12-15 15:06:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+const connectHistory = require("connect-history-api-fallback");
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(connectHistory());
 const port = process.env.PORT || 3000;
 app.listen(port);
